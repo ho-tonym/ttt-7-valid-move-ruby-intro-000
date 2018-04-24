51d30b4ce4e2e042d951f435587a9398117a43a6
Tony Ho <ho.tonym@gmail.com>
2018-04-24 02:00:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,11 +16,9 @@ end
 def valid_move? (board, index)
 
   if index.between?(0-10)
-    {
-      if position_taken?(board,index) == false
-        return true
-      end
-    }
+    if position_taken?(board,index) == false
+      return true
+    end
   else
     return false
   end
